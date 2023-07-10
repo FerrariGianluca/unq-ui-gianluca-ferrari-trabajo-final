2ef9c61373e3eac90e7dd933789425e2f9066f19
Gianluca <gianlucaferrari98@hotmail.com>
2023-07-10 18:52:56 -0300

    add home page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Game from './components/Game';
+import Home from './components/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Game />
+    <Home />
   </React.StrictMode>
 );
 
